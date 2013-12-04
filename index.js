392f5ed3359ba16f3da4d4d147ace55fb0c762da
bmac <bmac325@GMail.com>
2013-12-04 13:46:35 -0500

    update the reporter to save json and output the spec report

--- index.js
+++ index.js
@@ -1,132 +1,24 @@
+
 /**
  * Module dependencies.
  */
 
-/**
- * Initialize a new `Base` reporter.
- *
- * All other reporters generally
- * inherit from this reporter, providing
- * stats such as test duration, number
- * of tests passed / failed etc.
- *
- * @param {Runner} runner
- * @api public
- */
+var mocha = require('mocha'),
+    JSON = require('./json'),
+    Base = mocha.Base,
+    cursor = Base.cursor,
+    color = Base.color,
+    Spec = Base.Spec;
 
-function Base(runner) {
-  var self = this
-    , stats = this.stats = { suites: 0, tests: 0, passes: 0, pending: 0, failures: 0 }
-    , failures = this.failures = [];
-
-  if (!runner) return;
-  this.runner = runner;
-
-  runner.stats = stats;
-
-  runner.on('start', function(){
-    stats.start = new Date;
-  });
-
-  runner.on('suite', function(suite){
-    stats.suites = stats.suites || 0;
-    suite.root || stats.suites++;
-  });
-
-  runner.on('test end', function(test){
-    stats.tests = stats.tests || 0;
-    stats.tests++;
-  });
-
-  runner.on('pass', function(test){
-    stats.passes = stats.passes || 0;
-
-    var medium = test.slow() / 2;
-    test.speed = test.duration > test.slow()
-      ? 'slow'
-      : test.duration > medium
-        ? 'medium'
-        : 'fast';
-
-    stats.passes++;
-  });
-
-  runner.on('fail', function(test, err){
-    stats.failures = stats.failures || 0;
-    stats.failures++;
-    test.err = err;
-    failures.push(test);
-  });
-
-  runner.on('end', function(){
-    stats.end = new Date;
-    stats.duration = new Date - stats.start;
-  });
-
-  runner.on('pending', function(){
-    stats.pending++;
-  });
-}
-
-
-/**
- * Expose `JSON`.
- */
-
-exports = module.exports = TrainingReporter;
-
-/**
- * Initialize a new `JSON` reporter.
- *
- * @param {Runner} runner
- * @api public
- */
 
 function TrainingReporter(runner) {
-  var self = this;
   Base.call(this, runner);
+  Spec.call(this, runner);
+  JSON.call(this, runner);
+};
 
-  var tests = []
-    , failures = []
-    , passes = [];
 
-  runner.on('test end', function(test){
-    tests.push(test);
-  });
+Reporter.prototype.__proto__ = Spec.prototype;
 
-  runner.on('pass', function(test){
-    passes.push(test);
-  });
 
-  runner.on('fail', function(test){
-    failures.push(test);
-  });
-
-  runner.on('end', function(){
-    var obj = {
-        stats: self.stats
-      , tests: tests.map(clean)
-      , failures: failures.map(clean)
-      , passes: passes.map(clean)
-    };
-
-    console.log(JSON.stringify(obj, null, 2));
-  });
-}
-
-/**
- * Return a plain-object representation of `test`
- * free of cyclic properties etc.
- *
- * @param {Object} test
- * @return {Object}
- * @api private
- */
-
-function clean(test) {
-  return {
-      title: test.title
-    , fullTitle: test.fullTitle()
-    , duration: test.duration
-  }
-}
+exports = module.exports = TrainingReporter;
