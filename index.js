e1ab4ab9f6134940c23ceefc735b68e8ff8bd50a
Brendan McLoughlin <btmcloughlin@gmail.com>
2013-12-04 12:31:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,79 @@
-
 /**
  * Module dependencies.
  */
 
-var Base = require('./base')
-  , cursor = Base.cursor
-  , color = Base.color;
+/**
+ * Initialize a new `Base` reporter.
+ *
+ * All other reporters generally
+ * inherit from this reporter, providing
+ * stats such as test duration, number
+ * of tests passed / failed etc.
+ *
+ * @param {Runner} runner
+ * @api public
+ */
+
+function Base(runner) {
+  var self = this
+    , stats = this.stats = { suites: 0, tests: 0, passes: 0, pending: 0, failures: 0 }
+    , failures = this.failures = [];
+
+  if (!runner) return;
+  this.runner = runner;
+
+  runner.stats = stats;
+
+  runner.on('start', function(){
+    stats.start = new Date;
+  });
+
+  runner.on('suite', function(suite){
+    stats.suites = stats.suites || 0;
+    suite.root || stats.suites++;
+  });
+
+  runner.on('test end', function(test){
+    stats.tests = stats.tests || 0;
+    stats.tests++;
+  });
+
+  runner.on('pass', function(test){
+    stats.passes = stats.passes || 0;
+
+    var medium = test.slow() / 2;
+    test.speed = test.duration > test.slow()
+      ? 'slow'
+      : test.duration > medium
+        ? 'medium'
+        : 'fast';
+
+    stats.passes++;
+  });
+
+  runner.on('fail', function(test, err){
+    stats.failures = stats.failures || 0;
+    stats.failures++;
+    test.err = err;
+    failures.push(test);
+  });
+
+  runner.on('end', function(){
+    stats.end = new Date;
+    stats.duration = new Date - stats.start;
+  });
+
+  runner.on('pending', function(){
+    stats.pending++;
+  });
+}
+
 
 /**
  * Expose `JSON`.
  */
 
-exports = module.exports = JSONReporter;
+exports = module.exports = TrainingReporter;
 
 /**
  * Initialize a new `JSON` reporter.
@@ -20,7 +82,7 @@ exports = module.exports = JSONReporter;
  * @api public
  */
 
-function JSONReporter(runner) {
+function TrainingReporter(runner) {
   var self = this;
   Base.call(this, runner);
 
